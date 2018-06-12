8b2dbd41275b82760e030666729f1aca6cac4588
Tiago Alves <alvesjtiago@gmail.com>
2018-06-12 19:41:41 +0800

    Tests for array buffer.

---
+++ tests/unit/append-buffer.test.js
@@ -0,0 +1,11 @@
+import test from 'tape';
+import appendBuffer from '../../utils/append-buffer';
+
+test('should append last buffer element to first element', (t) => {
+  const mainBuffer = new Uint8Array([1, 2]).buffer;
+  const appendingBuffer = new Uint8Array([3, 4]).buffer;
+  const totalBuffer = appendBuffer(mainBuffer, appendingBuffer);
+  t.equal(4, totalBuffer.byteLength);
+  t.same(new Uint8Array(totalBuffer), new Uint8Array([1, 2, 3, 4]));
+  t.end();
+});
